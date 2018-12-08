c543382c9024fef01c1e0fa958fe9ff6d9acef1a
mbagro <44054417+mbagro@users.noreply.github.com>
2018-12-08 21:11:53 +0200

    Create 1.mm

---
+++ _biography/1.mm
@@ -0,0 +1,22 @@
+---
+author: mbagro
+title: "Timothy John Berners-Lee"
+image_url: /images/berners.jpg
+caption: " Είναι ο εφευρέτης του Παγκόσμιου Ιστού - World Wide Web"
+categories:
+  - biography
+  - definition
+tags:
+  - Timothy John Berners-Lee
+  - Παγκόσμιος Ιστός
+  - Ιntenet
+  - World Wide Web
+gallery:
+  - image_url: /images/berners.png
+    caption: "Ο Σερ Τίμοθι Τζον Μπέρνερς Λι (Sir Timothy John Berners-Lee, 8 Ιουνίου 1955), μέλος του Τάγματος της Αξίας, Ιππότης-Διοικητής του Τάγματος της Βρετανικής Αυτοκρατορίας, Μέλος της Βασιλικής Εταιρείας, Μέλος της Βασιλικής Εταιρείας Μηχανικής, Μέλος της Βασιλικής Εταιρείας Τεχνών είναι ο εφευρέτης του Παγκόσμιου Ιστού (World Wide Web), διευθυντής της κοινοπραξίας World Wide Web"
+  
+    
+---
+
+{% include_relative bio-berners.md %}
+
