da44a3c8b75afe0b299cbbe5fa2c3897f9ccbb37
Valia Gountenoudi  Π2015090 <32698824+p15gkou@users.noreply.github.com>
2020-03-29 23:10:35 +0300

    fingerprint scanner

---
+++ _gallery/fingerprint scanner.md/script_1.js
@@ -0,0 +1,16 @@
+
+
+---
+author:  p15gkou
+title:   fingerprint-scanner
+image_url: /images/fingerprint scanner.jpg
+caption: 'Σαρωτής δακτυλικών αποτυπωμάτων στο πίσω μέρος ενός smartphone.Mε αυτόν τον τρόπο επιτρέπεται 
+ το γρήγορο ξεκλείδωμα της συσκευής και μηδενίζει την πιθανότητα να πατήσει λάθος κωδικό ο χρήστης ή
+να επιτραπεί η είσοδος σε μη εξουσιοδοτημένους χρήστες .'
+license_url: 'https://commons.wikimedia.org/wiki/File:Xiaomi_Redmi_Note_3_fingerprint_scanner.jpg'
+license_text: Courtesy of wikimedia
+categories:
+  - input devices
+tags:
+  - sensor
+---
