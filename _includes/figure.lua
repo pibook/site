7df8490bf20ae551bf3dec6e9f755762d19523d2
epidrome <github@epidro.me>
2022-01-11 19:24:57 +0200

    added quotes

--- _includes/figure.lua
+++ _includes/figure.lua
@@ -1,5 +1,3 @@
-stringify = pandoc.utils.stringify
-
 function Image(img)
       if img.classes:find('figure',1) then
         local fn = img.src
@@ -9,7 +7,7 @@ function Image(img)
         --print(fn)
         local src=pandoc.utils.stringify(doc.meta.image_url) or "src has not been set"
         local caption=pandoc.utils.stringify(doc.meta.caption) or "caption has not been set"
-        local figure = "{% include figure image_path=" .. src .. " caption=" .. caption .. " %}"
+        local figure = "{% include figure image_path=\"" .. src .. "\" caption=\"" .. caption .. "\" %}"
         return pandoc.RawInline('markdown',figure)
       end
 end
