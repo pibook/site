9b6eb2689fbba0bfaf33912bbf96e6e9849838aa
epidrome <github@epidro.me>
2022-01-11 20:50:49 +0200

    added figures to bios

--- _includes/make-text.sh
+++ _includes/make-text.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 for filename in extras/cs-*.md; do
+   [ -e "$filename" ] || continue
+   pandoc "$filename" --lua-filter=figure.lua --to markdown > ./"$(basename "$filename")"
+done
+
+for filename in extras/bio-*.md; do
    [ -e "$filename" ] || continue
    pandoc "$filename" --lua-filter=figure.lua --to markdown > ./"$(basename "$filename")"
 done
