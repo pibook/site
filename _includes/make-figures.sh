6f93da1bdcac8a00b5bdfd2668b46da0ec8f35d9
epidrome <github@epidro.me>
2022-01-14 14:47:06 +0200

    lua filter adds fig id to work with mm figure

--- _includes/make-figures.sh
+++ _includes/make-figures.sh
@@ -2,10 +2,11 @@
 
 for filename in extras/cs-*.md; do
    [ -e "$filename" ] || continue
-   pandoc "$filename" --lua-filter=figure-id.lua --to markdown | pandoc --filter pandoc-fignos --to html > ./"$(basename "$filename")"
+   pandoc "$filename" --lua-filter=figure-id.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --lua-filter mm-figure.lua --to markdown > ./"$(basename "$filename")"
 done
 
 for filename in extras/bio-*.md; do
    [ -e "$filename" ] || continue
-   pandoc "$filename" --lua-filter=figure-id.lua --to markdown | pandoc --filter pandoc-fignos --to html > ./"$(basename "$filename")"
+   pandoc "$filename" --lua-filter=figure-id.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --lua-filter mm-figure.lua --to markdown > ./"$(basename "$filename")"
 done
+
