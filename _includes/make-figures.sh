4bfed860c5fc9c2eb1ed75065ed2a6359d24f445
epidrome <github@epidro.me>
2024-08-12 16:03:51 +0200

    removed figure element

--- _includes/make-figures.sh
+++ _includes/make-figures.sh
@@ -2,16 +2,16 @@
 
 for filename in extras/cs-*.md; do
    [ -e "$filename" ] || continue
-   pandoc "$filename" --lua-filter=figure-id.lua --to markdown | pandoc --lua-filter mm-figure.lua --to markdown | pandoc --filter pandoc-crossref --to markdown > ./"$(basename "$filename")"
+   pandoc "$filename" --lua-filter=figure-id.lua --to markdown | pandoc --filter pandoc-crossref --to markdown > ./"$(basename "$filename")"
 done
 
 for filename in extras/bio-*.md; do
    [ -e "$filename" ] || continue
-   pandoc "$filename" --lua-filter=figure-id.lua --to markdown | pandoc --lua-filter mm-figure.lua --to markdown | pandoc --filter pandoc-crossref --to markdown > ./"$(basename "$filename")"
+   pandoc "$filename" --lua-filter=figure-id.lua --to markdown | pandoc --filter pandoc-crossref --to markdown > ./"$(basename "$filename")"
 done
 
 for filename in text/ch0*.txt; do
    [ -e "$filename" ] || continue
-   pandoc "$filename" --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=remove-include.lua --to markdown | pandoc --lua-filter=figure-id.lua --to markdown | pandoc --lua-filter mm-figure.lua --to markdown | pandoc --filter pandoc-crossref --to markdown | tail -n +2 > ./"$(basename "$filename" .txt).md"
+   pandoc "$filename" --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=remove-include.lua --to markdown | pandoc --lua-filter=figure-id.lua --to markdown | pandoc --filter pandoc-crossref --to markdown | tail -n +2 > ./"$(basename "$filename" .txt).md"
 done
 
